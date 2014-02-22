17a6e2618984a48976dbc3ce02df4c27378e5006
Alex Eftimie <alex@eftimie.ro>
2014-02-22 13:36:00 +0200

    Fix styles

--- fabfile.py
+++ fabfile.py
@@ -2,13 +2,13 @@ from fabric.api import local
 
 
 def devel():
-    local('jekyll --auto --serve')
+    local('jekyll server --auto ')
 
 
 def build():
-    local('jekyll')
+    local('jekyll build')
 
 
 def deploy():
     build()
-    local('rsync -rtv _site/ site@rosedu.org:lists.rosedu.org/')
+    local('rsync -rtv _site/ site@rosedu.org:lists.rosedu.org/ --exclude env')
